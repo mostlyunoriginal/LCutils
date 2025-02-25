556f7a240c7631e51e833be0b0707fa28195b71f
mostlyunoriginal <lance.couzens@gmail.com>
2025-02-25 13:27:47 -0700

    Initial commit

---
+++ R/freqy.R
@@ -0,0 +1,77 @@
+#' freqy - a function for SAS PROC FREQ-like frequency tables
+#' --requires dplyr and rlang
+#'
+#' @param df a data frame or tibble
+#' @param ... zero or more variables to cross for frequency cells
+#' @param where an optional where clause
+#' @param missincl TRUE/FALSE should include records with NA in one or more vars?
+#' @param digits integerish number of digits after the decimal for percentages
+#'
+#' @return a data frame
+#' @export
+#'
+#' @examples
+#' freqy(mtcars)
+#' freqy(mtcars,cyl)
+#' freqy(mtcars,cyl,gear)
+#' freqy(mtcars,cyl,gear,where=hp>100)
+#' freqy(mtcars,cyl,gear,digits=3)
+#'
+freqy<-function(df,...,where=NULL,missincl=TRUE,digits=1){
+
+  pd<-require(dplyr,quietly=TRUE)
+  pr<-require(rlang,quietly=TRUE)
+
+  if (pd==FALSE){
+
+    stop("dplyr package required but not installed")
+
+  } else if (pr==FALSE){
+
+    stop("rlang package required but not installed")
+
+  } else if (!(is.data.frame(df)|is_tibble(df))) {
+
+    stop("`df` must be a tibble or data frame")
+
+  } else if (!rlang::is_logical(missincl)) {
+
+    stop("`missincl` must be TRUE or FALSE")
+
+  } else if (!rlang::is_integerish(digits)) {
+
+    stop("`digits` must be a whole number")
+
+  } else {
+
+    if (missing(where)) where=expr(TRUE)
+
+    vars<-enquos(...)
+
+    if (missincl==F){
+
+      missexp=expr(!if_any(.cols=c(!!!vars),.fn=~is.na(.x)))
+
+    } else missexp=expr(TRUE)
+
+    df %>%
+      dplyr::ungroup() %>%
+      dplyr::filter({{where}} & eval(missexp)) %>%
+      dplyr::mutate(N=n()) %>%
+      dplyr::group_by(...) %>%
+      dplyr::summarize(
+        .groups="drop"
+        ,n=formatC(n(),big.mark=',',format="f",width=8,digits=0)
+        ,prepct=100*n()/mean(N)
+        ,pren=n()
+      ) %>%
+      dplyr::mutate(
+        pct=formatC(prepct,format="f",digits=.env$digits,width=10)
+        ,cumn=formatC(cumsum(pren),big.mark=',',format="f",width=8,digits=0)
+        ,cumpct=formatC(cumsum(prepct),format="f",digits=.env$digits,width=10)
+      ) %>%
+      dplyr::select(-pren,-prepct) %>%
+      as.data.frame()
+
+  }
+}
---
+++ R/meanit.R
@@ -0,0 +1,124 @@
+#' meanit - a function inspired by SAS PROC MEANS
+#' --requires dplyr, tidyr, and rlang
+#'
+#' @param df a data frame or tibble
+#' @param anvar a required outcome variable to analyze
+#' @param ... zero or more variables to cross-classify analysis by
+#' @param where an optional where clause
+#' @param missincl RUE/FALSE should include records with NA in one or more vars?
+#' @param digits integerish number of digits after the decimal for percentages
+#' @param center either of "mean" (default) or "median"
+#'
+#' @return a data frame
+#' @export
+#'
+#' @examples
+#' meanit(mtcars,mpg)
+#' meanit(mtcars,mpg,cyl,gear)
+#' meanit(mtcars,c(mpg,hp),cyl,gear)
+#' meanit(mtcars,mpg,cyl,where=hp>100)
+#' meanit(mtcars,hp,cyl,center="median")
+#' meanit(mtcars,hp,cyl,digits=3)
+#'
+meanit<-function(df,anvar,...,where=NULL,missincl=TRUE,digits=1,center=c("mean","median")){
+
+  pd<-require(dplyr,quietly=TRUE)
+  pr<-require(rlang,quietly=TRUE)
+  pt<-require(tidyr,quietly=TRUE)
+
+  if (pd==FALSE){
+
+    stop("dplyr package required but not installed")
+
+  } else if (pr==FALSE){
+
+    stop("rlang package required but not installed")
+
+  } else if (pt==FALSE){
+
+    stop("tidyr package required but not installed")
+
+  } else if (!(is.data.frame(df)|is_tibble(df))) {
+
+    stop("`df` must be a tibble or data frame")
+
+  } else if (missing(anvar)) {
+
+    stop("`anvar` is missing with no default")
+
+  } else if (!rlang::is_logical(missincl)) {
+
+    stop("`missincl` must be TRUE or FALSE")
+
+  } else if (!rlang::is_integerish(digits)) {
+
+    stop("`digits` must be a whole number")
+
+  } else {
+
+    rlang::arg_match(center)
+
+    center<-center[1]
+
+    if (missing(where)) where=expr(TRUE)
+
+    vars<-enquos(...)
+
+    if (missincl==F){
+
+      missexp=expr(!if_any(.cols=c(!!!vars),.fn=~is.na(.x)))
+
+    } else missexp=expr(TRUE)
+
+    if (center=="mean"){
+
+      c.select=expr(c(ends_with("_mean"),ends_with("_sd")))
+
+    } else if (center=="median"){
+
+      c.select=expr(c(ends_with("_q25"),ends_with("_median"),ends_with("_q75")))
+
+    }
+
+    df %>%
+      dplyr::ungroup() %>%
+      dplyr::filter({{where}} & eval(missexp)) %>%
+      dplyr::mutate(N=n()) %>%
+      dplyr::group_by(...) %>%
+      dplyr::summarize(
+        .groups="drop"
+        ,n=formatC(n(),digits=0,format="f",big.mark=',',width=8)
+        ,prepct=100*n()/mean(N)
+        ,pren=n()
+        ,across(
+          .cols={{anvar}}
+          ,.fns=list(
+            nomiss=~formatC(sum(!is.na(.x)),big.mark=',',format="f",digits=0,width=10)
+            ,nmiss=~formatC(sum(is.na(.x)),big.mark=',',format="f",digits=0,width=10)
+            ,min=~formatC(ifelse(sum(!is.na(.x))>0,min(.x,na.rm=T),NA_real_),big.mark=',',format="f",digits=.env$digits,width=10)
+            ,q25=~formatC(ifelse(sum(!is.na(.x))>0,quantile(.x,0.25,na.rm=T),NA_real_),big.mark=',',format="f",digits=.env$digits,width=10)
+            ,median=~formatC(ifelse(sum(!is.na(.x))>0,quantile(.x,0.50,na.rm=T),NA_real_),big.mark=',',format="f",digits=.env$digits,width=10)
+            ,mean=~formatC(ifelse(sum(!is.na(.x))>0,mean(.x,na.rm=T),NA_real_),big.mark=',',format="f",digits=.env$digits,width=10)
+            ,sd=~formatC(ifelse(sum(!is.na(.x))>0,sd(.x,na.rm=T),NA_real_),big.mark=',',format="f",digits=.env$digits,width=10)
+            ,q75=~formatC(ifelse(sum(!is.na(.x))>0,quantile(.x,0.75,na.rm=T),NA_real_),big.mark=',',format="f",digits=.env$digits,width=10)
+            ,max=~formatC(ifelse(sum(!is.na(.x))>0,max(.x,na.rm=T),NA_real_),big.mark=',',format="f",digits=.env$digits,width=10)
+          )
+        )
+      ) %>%
+      dplyr::mutate(
+        pct=formatC(prepct,format="f",digits=.env$digits,width=10)
+        ,cumn=formatC(cumsum(pren),digits=0,format="f",big.mark=',',width=8)
+        ,cumpct=formatC(cumsum(prepct),format="f",digits=.env$digits,width=10)
+      ) %>%
+      dplyr::select(...,n,pct,cumn,cumpct,ends_with("_nomiss"),ends_with("_nmiss"),ends_with("_min"),eval(c.select),ends_with("_max")) %>%
+      tidyr::pivot_longer(
+        -c(!!!vars,n,pct,cumn,cumpct)
+        ,names_to=c("variable",".value")
+        ,names_pattern="^(.+)_(nomiss|nmiss|min|q25|mean|sd|median|q75|max)$"
+      ) %>%
+      dplyr::arrange(variable,...) %>%
+      dplyr::select(variable,...,everything()) %>%
+      as.data.frame()
+
+  }
+}
